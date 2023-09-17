2320460c773323daf0fe55588b213df4f91c3a5f
suhag-alamin <developersuhag@gmail.com>
2023-09-17 22:29:11 +0600

    update

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,23 @@
 import { Server } from 'http';
 import app from './app';
 import config from './config';
-import { errorlogger, logger } from './shared/logger';
 
-
-async function bootstrap() {
-
+async function main() {
   const server: Server = app.listen(config.port, () => {
-    logger.info(`Server running on port ${config.port}`);
+    console.log(`Server running on port ${config.port}`);
   });
 
   const exitHandler = () => {
-
     if (server) {
       server.close(() => {
-        logger.info('Server closed');
+        console.log('Server closed');
       });
     }
     process.exit(1);
   };
 
   const unexpectedErrorHandler = (error: unknown) => {
-    errorlogger.error(error);
+    console.log(error);
     exitHandler();
   };
 
@@ -29,11 +25,11 @@ async function bootstrap() {
   process.on('unhandledRejection', unexpectedErrorHandler);
 
   process.on('SIGTERM', () => {
-    logger.info('SIGTERM received');
+    console.log('SIGTERM received');
     if (server) {
       server.close();
     }
   });
 }
 
-bootstrap();
+main();
